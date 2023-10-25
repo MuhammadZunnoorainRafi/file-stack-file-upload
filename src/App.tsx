2079ab4708649108b0888bcb431587a109b67b2f
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-10-25 23:13:28 +0500

    UI setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col items-center gap-y-10">
+      <h1 className="font-mono text-xl font-bold tracking-wider">
+        Image upload using File-Stack 👍
+      </h1>
+      <button className="px-4 py-1 bg-blue-500 text-white hover:bg-blue-700 active:bg-blue-900 transition-colors active:scale-[0.97]">
+        Upload
+      </button>
+      <h1 className="font-bold text-lg">Images:</h1>
+    </div>
+  );
 }
 
 export default App;
