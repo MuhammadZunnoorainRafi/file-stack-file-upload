368ff99706d27ffd8258dea8283dae160b601792
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-10-25 23:48:28 +0500

    PickerOverlay added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { PickerOverlay } from 'filestack-react';
+import { useState } from 'react';
+
 function App() {
+  const [showPicker, setShowPicker] = useState(false);
   return (
-    <div className="flex flex-col items-center gap-y-10">
+    <div className="flex mt-32 flex-col items-center gap-y-10">
       <h1 className="font-mono text-xl font-bold tracking-wider">
         Image upload using File-Stack 👍
       </h1>
-      <button className="px-4 py-1 bg-blue-500 text-white hover:bg-blue-700 active:bg-blue-900 transition-colors active:scale-[0.97]">
+      {showPicker && (
+        <div className="absolute">
+          <PickerOverlay
+            apikey={import.meta.env.VITE_API_KEY}
+            pickerOptions={{
+              accept: ['image/*'],
+              onClose: () => setShowPicker(false),
+            }}
+          />
+        </div>
+      )}
+      <button
+        onClick={() => setShowPicker(true)}
+        className="px-4 rounded-md py-1 bg-blue-500 text-white hover:bg-blue-700 active:bg-blue-900 transition-colors"
+      >
         Upload
       </button>
       <h1 className="font-bold text-lg">Images:</h1>
